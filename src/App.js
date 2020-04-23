a180811a235d37471b1ae1043144247f32f21e8f
Leo  Antelyes <lantelyes@quuantstamp.com>
2020-04-23 17:32:38 -0400

    show version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './eth-logo.svg';
 import './App.css';
+import Web3 from 'web3';
 
-function App() {
+class App extends Component {
+
+  state={web3Version: null}
+
+  componentDidMount() {
+
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Injected Web3 Version: {Web3.version}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
-
+}
 export default App;
